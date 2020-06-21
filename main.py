50ea9fb0bb8b5e2a351bd97d0c599ed5550fb15d
Jan Moravec <moravecjan@email.cz>
2020-06-21 20:07:50 +0200

    First version of evaluation

--- main.py
+++ main.py
@@ -25,9 +25,47 @@ def main():
     connections = ConnectionsAccess(results_stops)
 
     print("Calculating all connection paths and times.")
-    calculation = PathCalculations(connections, "Na Pískách")
-    calculation.find_all_connections()
-    calculation.save()
+    start_stops = ["Na Pískách", "Kudrnova", "Branické náměstí", "Sídliště Malešice"]
+    all_paths = get_all_paths(connections, start_stops)
+
+    print("Evaluating stops.")
+    stops = connections.get_stops()
+    scores = evaluate_paths(stops, all_paths)
+    print(scores)
+
+
+def get_all_paths(connections: ConnectionsAccess, start_stops: list):
+    all_paths = []
+    for start_stop in start_stops:
+        calculation = PathCalculations(connections, start_stop)
+        calculation.find_all_connections()
+        calculation.save()
+
+        results_list = calculation.get_results()
+        results_dict = {}
+        for (stop, distance, path) in results_list:
+            results_dict[stop] = {"distance_min": distance, "path": path}
+
+        print(results_dict["Na Pískách"])
+        all_paths.append(results_dict)
+
+    return all_paths
+
+
+def evaluate_paths(stops: list, all_paths: list):
+    scores = []
+    for stop in stops:
+        score = 0
+
+        for path in all_paths:
+            if not stop in path:
+                break
+            score += path[stop]["distance_min"]
+
+        scores.append((stop, score))
+
+    scores.sort(key=lambda tup: tup[1])
+    return scores
 
 
 if __name__ == "__main__":
