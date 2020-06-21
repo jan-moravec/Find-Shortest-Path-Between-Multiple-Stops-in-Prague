8b19fc5ace88acdd69c40d777b3ff1c4e22940ac
Jan Moravec <moravecjan@email.cz>
2020-06-21 20:46:46 +0200

    Update of evaluation and result print

--- main.py
+++ main.py
@@ -24,14 +24,24 @@ def main():
     print("Creating class for accessing results")
     connections = ConnectionsAccess(results_stops)
 
-    print("Calculating all connection paths and times.")
+    print("Calculating all connection paths and times")
     start_stops = ["Na Pískách", "Kudrnova", "Branické náměstí", "Sídliště Malešice"]
     all_paths = get_all_paths(connections, start_stops)
 
-    print("Evaluating stops.")
+    print("Evaluating stops")
     stops = connections.get_stops()
-    scores = evaluate_paths(stops, all_paths)
-    print(scores)
+    #scores = evaluate_paths(stops, all_paths, sum)
+    scores = evaluate_paths(stops, all_paths, score_function)
+
+    print("Printing results")
+    for (index, value) in enumerate(scores):
+        print(f"{index}: {value[0]}")
+
+        for path in all_paths:
+            print(f"\t - {path[value[0]]['path']}")
+
+        if index == 20:
+            break
 
 
 def get_all_paths(connections: ConnectionsAccess, start_stops: list):
@@ -46,26 +56,35 @@ def get_all_paths(connections: ConnectionsAccess, start_stops: list):
         for (stop, distance, path) in results_list:
             results_dict[stop] = {"distance_min": distance, "path": path}
 
-        print(results_dict["Na Pískách"])
         all_paths.append(results_dict)
 
     return all_paths
 
 
-def evaluate_paths(stops: list, all_paths: list):
+def evaluate_paths(stops: list, all_paths: list, score_function):
     scores = []
     for stop in stops:
-        score = 0
+        distances = []
 
         for path in all_paths:
             if not stop in path:
                 break
-            score += path[stop]["distance_min"]
+            distances.append(path[stop]["distance_min"])
 
-        scores.append((stop, score))
+        if len(distances) == len(all_paths):
+            score = score_function(distances)
+            scores.append((stop, score))
 
     scores.sort(key=lambda tup: tup[1])
     return scores
+
+
+def score_function(values: list):
+    sum_value = sum(values)
+    mean_value = sum_value / len(values)
+    deviations = [(x - mean_value)**2 for x in values]
+
+    return sum_value + sum(deviations)**0.5
 
 
 if __name__ == "__main__":
